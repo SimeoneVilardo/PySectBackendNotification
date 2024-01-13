241b0d98a21ed5ea333c1c7e722967427aca70d1
SimeoneVilardo <simeone.vilardo@gmail.com>
2024-01-13 19:09:25 +0100

    Move connection creation inside API

--- server/helpers/queue-helper.js
+++ server/helpers/queue-helper.js
@@ -1,10 +1,8 @@
 const queueHelper = {};
 const redis = require('redis');
 
-queueHelper.client = redis.createClient({ url: 'redis://pysect-backend-redis:6379' });
-
 queueHelper.subscribe = async (user, callback) => {
-    var subscriber = queueHelper.client.duplicate();
+    var subscriber = redis.createClient({ url: 'redis://pysect-backend-redis:6379' });
     subscriber.user = user;
     await subscriber.connect();
     await subscriber.subscribe(user.username, (message) => {
